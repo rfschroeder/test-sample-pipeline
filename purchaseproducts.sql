e55b1303adf40dd1205ca8da29af192c7a161065
rfschroeder <renanfernandos@gmail.com>
2024-03-01 10:19:01 -0300

    Update purchaseproducts.sql

--- purchaseproducts.sql
+++ purchaseproducts.sql
@@ -1,14 +1,14 @@
 select
-	stg.orderid as mdmorderid,
-        stg.customerid as mdmcustomerid,
-        stg.customername as mdmcustomername,
-	product.bar_code as mdmbarcode,
-	stg_p.productname as mdmproductname
+    stg.orderid as mdmorderid,
+    stg.customerid as mdmcustomerid,
+    stg.customername as mdmcustomername,
+    product.bar_code as mdmbarcode,
+    stg_p.productname as mdmproductname
     --metadata-v2--
 from
-	stg_nlp_purchaseorder stg
+    stg_nlp_purchaseorder stg
 join UNNEST(stg.products) as product
 join stg_nlp_product stg_p on
-	(product.bar_code = stg_p.bar_code)
+    (product.bar_code = stg_p.bar_code)
 --timestamp-- and stg.mdmCounterForEntity > {{start_from}}
 --tenantIds-- and mdmTenantId IN {{tenantIds}}
