4b428ccb67f1093dfcf25215d8c5999a59334142
Renan Schroeder <renanfernandos@gmail.com>
2025-02-21 10:23:15 -0300

    Create purchaseproducts2.sql

---
+++ purchaseproducts2.sql
@@ -0,0 +1,14 @@
+select
+    stg.orderid as mdmorderid,
+    stg.customerid as mdmcustomerid,
+    stg.customername as mdmcustomername,
+    product.bar_code as mdmbarcode,
+    stg_p.productname as mdmproductname
+    --metadata-v2--
+from
+    stg_nlp_purchaseorder stg
+join UNNEST(stg.products) as product
+join {{tableName_product2}} stg_p on -- stg_nlp_product
+    (product.bar_code = stg_p.bar_code)
+--timestamp-- and stg.mdmCounterForEntity > {{start_from}}
+--tenantIds-- and mdmTenantId IN {{tenantIds}}
