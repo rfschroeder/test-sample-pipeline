02da2f69384c789d8b2e34ded4513681806cee25
Renan Schroeder <renanfernandos@gmail.com>
2025-02-25 15:17:03 -0300

    Update purchaseproducts.sql

--- purchaseproducts.sql
+++ purchaseproducts.sql
@@ -7,8 +7,8 @@ select
     --metadata-v2--
 from
     stg_nlp_purchaseorder stg
-join UNNEST(stg.products) as product
-join {{tableName_product}} stg_p on -- stg_nlp_product
+left join UNNEST(stg.products) as product
+left join {{tableName_product}} stg_p on -- stg_nlp_product
     (product.bar_code = stg_p.bar_code)
 --timestamp-- and stg.mdmCounterForEntity > {{start_from}}
 --tenantIds-- and mdmTenantId IN {{tenantIds}}
